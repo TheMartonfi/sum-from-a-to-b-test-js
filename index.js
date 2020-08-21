2db17731dbd3fde96b857029e004e6453557a1ee
TheMartonfi <carlmartonfi@hotmail.com>
2020-08-21 23:05:12 +0000

    Added recursive function

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+
+  if (fromN !== toN + 1) {
+    return fromN + sum(fromN + 1, toN);
+  } else {
+    return 0;
+  }
+};
 
 module.exports = sum;
